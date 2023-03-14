50f4d96f8d4586c4e57495f2e95c93e6ce1aefa1
JeevanGarg <luckyjeevangarg89@gmail.com>
2023-03-14 15:34:33 +0530

    Electronic Store User Service

--- src/main/java/com/ElectronicStore/ElectronicStoreApplication.java
+++ src/main/java/com/ElectronicStore/ElectronicStoreApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElectronicStoreApplication
+public class
+ElectronicStoreApplication
 {
 	public static void main(String[] args)
 	{
