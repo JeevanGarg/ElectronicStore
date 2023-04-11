dea18a1c608222bea31891488afa6c964360a9e8
JeevanGarg <luckyjeevangarg89@gmail.com>
2023-04-11 13:05:07 +0530

    cart service

--- src/test/java/com/ElectronicStore/ElectronicStoreApplicationTests.java
+++ src/test/java/com/ElectronicStore/ElectronicStoreApplicationTests.java
@@ -1,29 +1,12 @@
 package com.ElectronicStore;
 
-
-import com.ElectronicStore.dtos.CategoryDto;
-import com.ElectronicStore.exceptions.ResourceNotFoundException;
-import com.ElectronicStore.services.CategoryService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
-
 
 @SpringBootTest
 class  ElectronicStoreApplicationTests
 {
-    @Autowired
-    private CategoryService categoryService;
 
-    @Test
-    public void getCategoryTest() throws ResourceNotFoundException {
-        CategoryDto categoryDto=this.categoryService.get("bc73f1bd-6819-4be6-957b-ce819666fc8e");
 
-        Assertions.assertEquals("Electronics",categoryDto.getTitle());
-    }
 
 }
 
